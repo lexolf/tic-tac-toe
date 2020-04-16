58ee36ee1f74658dd259365b93034924d4eab43b
Alexey Volkov <grulfex@gmail.com>
2020-04-17 01:32:13 +0300

    Create empty factory and module

---
+++ script.js
@@ -0,0 +1,15 @@
+const playerFactory = (name) => {
+
+}
+
+const gameBoard = (() => {
+    const addX = (pos) => 'X';
+    const addO = (pos) => 'O'
+    return {
+        addX,
+        addO,
+    };
+})();
+
+// I actually have no idea what I am doing right now 
+// Sleeping might help
