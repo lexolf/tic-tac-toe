300094f3c3cf38ad3c2987cbe7407971d0e52203
Alexey Volkov <grulfex@gmail.com>
2020-04-17 19:44:49 +0300

    Create starting point

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const playerFactory = (name) => {
-
-}
-
-const gameBoard = (() => {
-    const addX = (pos) => 'X';
-    const addO = (pos) => 'O'
-    return {
-        addX,
-        addO,
+const Player = (name) => {
+    const getName = () => name;
+    const markSymbol = () => 'X';
+    const canMark = () => {}
+    const changeTurn = canMark => {
+        canMark = !canMark;
     };
-})();
+    return { getName };
+} 
 
-// I actually have no idea what I am doing right now 
-// Sleeping might help
+const game = (() => {
+    const getCell = (index) => {
+        let cells = document.getElementsByClassName("game__cell");
+        let cell = cells[index];
+        return { cell }
+    };
+
+    for(let i = 0; i < 9; i++){
+        getCell(i).cell.addEventListener("click", e = () => {alert("Clicked" + getCell(i).cell.classList)});
+    }
+
+    return {getCell}
+})();
