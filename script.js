c75cc64a1ae8e7c04457abf369951d6ed74adae4
Alexey Volkov <grulfex@gmail.com>
2020-04-17 22:03:42 +0300

    Play around with functions to understand this

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const Player = (name) => {
     const getName = () => name;
-    const markSymbol = () => 'X';
-    const canMark = () => {}
-    const changeTurn = canMark => {
-        canMark = !canMark;
-    };
     return { getName };
 } 
 
 const game = (() => {
+
+    // Add ability to find a specific cell on the board
+
     const getCell = (index) => {
         let cells = document.getElementsByClassName("game__cell");
         let cell = cells[index];
         return { cell }
-    };
-
-    for(let i = 0; i < 9; i++){
-        getCell(i).cell.addEventListener("click", e = () => {alert("Clicked" + getCell(i).cell.classList)});
     }
 
-    return {getCell}
+    // Mark cell with symbol
+
+    const mark = (i) => {
+        getCell(i).cell.textContent = "X";
+    }
+
+    // Make every cell responsive to user input 
+    
+    for(let i = 0; i < 9; i++){
+        getCell(i).cell.addEventListener("click", e = () => {mark(i)});
+    }
 })();
