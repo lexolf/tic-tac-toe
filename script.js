c90188cd8e1e9a62e940602e485afa620599fc5d
Alexey Volkov <grulfex@gmail.com>
2020-04-19 16:51:25 +0300

    Create players with start menu

--- script.js
+++ script.js
@@ -7,8 +7,44 @@ const game = (() => {
 
     // Create modal for new game options
 
-    const renderModal = () => {
-        console.log('hey')
+    const renderMenu = () => {
+        let menu = document.getElementsByClassName("menu")[0];
+        let menuOverlay = document.getElementsByClassName("menu-overlay")[0];
+        switch(menu.style.display){
+            case "none":
+                menu.style.display = "block";
+                menuOverlay.style.display = "block";
+                break
+            case "block":
+                menu.style.display = "none";
+                menuOverlay.style.display = "none";
+                break
+            default:
+                menu.style.display = "block";
+                menuOverlay.style.display = "block";
+        }
+    }
+
+    // Create players one and two 
+
+    const createPlayers = (playerOneInput, playerTwoInput) => {
+        let playerOne = Player(playerOneInput.value);
+        let playerTwo = Player(playerTwoInput.value);
+        console.log(playerOne.getName());
+        console.log(playerTwo.getName());
+    }
+
+    // Close modal and start game upon entering player names 
+
+    const startGame = () => {
+        let playerOneInput = document.getElementsByClassName('menu__player')[0];
+        let playerTwoInput = document.getElementsByClassName('menu__player')[1];
+        if(playerOneInput.value != "" && playerTwoInput.value != ""){
+            renderMenu();
+            createPlayers(playerOneInput, playerTwoInput);
+        } else {
+            console.log("Player names are required!");
+        }
     }
 
     // Add ability to find a specific cell on the board
@@ -34,6 +70,11 @@ const game = (() => {
     // Make new game button active
 
     let newGameButton = document.getElementsByClassName("game__button--new")[0];
-    newGameButton.addEventListener("click", renderModal, false);
+    newGameButton.addEventListener("click", renderMenu, false);
+
+    // Menu button starts the game and closes menu
+
+    let startGameButton = document.getElementsByClassName("menu__start")[0];
+    startGameButton.addEventListener("click", startGame, false)
 
 })();
